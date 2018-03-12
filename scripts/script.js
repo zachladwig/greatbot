540df615112b66869d6f9dd51215c8ab3dea0547
Zach Ladwig <zladwig@slack-corp.com>
2018-03-12 06:04:01 +0000

    niche

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,6 @@ const dogFacts = require('dog-facts');
 
 module.exports = function(robot) {
 
-  let good = ['Excellent!', 'Superb!', 'Wonderful!']
   let adorableDog = 'https://media.giphy.com/media/7Jkv02RLFYj6M/giphy.gif'
   let partyTime = 'https://www.wcr.org/media/83d19a17-c3d9-43fe-9e8d-47b7175b25bcParty-Time1.png'
 
@@ -24,23 +23,16 @@ module.exports = function(robot) {
 
   robot.hear(/dog/, function(msg) {
     let randomFact = dogFacts.random();
-    msg.send(randomFact)
+    msg.send(randomFact);
+    msg.send(adorableDog);
   })
 
-  robot.hear(/dog picture/, function(msg) {
-    msg.send(adorableDog)
-  })
-
-  robot.respond(/are you there?/i, function(msg){
-    msg.reply('Yes, usually here, and listening.');
-  });
-
-  robot.respond(/is it time to party?/i, function(msg){
+  robot.respond(/is it party time?/i, function(msg){
     let random = Math.random()
     let text;
 
     if(random > 0.9) {
-      text ="https://gph.is/1IH3RW6"
+      text = "https://gph.is/1IH3RW6"
     }
     else {
       text = "https://media.giphy.com/media/GhnctUOrT8HBe/giphy.gif"
@@ -56,30 +48,3 @@ module.exports = function(robot) {
   })
 
 }
-
-
-
-
-
-//
-//
-//     robot.respond(/is it a (weekend|holiday)\s?\?/i, function(msg){
-//         var today = new Date();
-//
-//         msg.reply(today.getDay() === 0 || today.getDay() === 6 ? "YES" : "NO");
-//     });
-//
-//     robot.hear(/i did it/i, function(msg){
-//         msg.send("Congratulations! Good job!");
-//     });
-//
-//     robot.respond(/are you there?/i, function(msg){
-//         msg.reply('Yes, usually here, and listening.');
-//     });
-//
-//     robot.respond(/convert \$(.*) to btc/i, function(res){
-//         var usd = res.match[1];
-//         res.reply('That is about ' + usd * 0.0024 + ' in BTC');
-//     });
-//
-// }
